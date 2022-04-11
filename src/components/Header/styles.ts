1842c1c26dded9a43d1da5a91654371f86160571
roquiles <roberta_btg@hotmail.com>
2022-04-11 13:57:05 -0300

    Adjust styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,43 +6,54 @@ export const Container = styled.div`
   width: 100%;
   height: 20rem;
   background: var(--header-bg);
-  padding: 0 8rem;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  padding-bottom: 3rem;
+  gap: 1.5rem;
 
   strong {
     letter-spacing: 0.1rem;
-    font-size: 4rem;
+    text-align: center;
+    font-size: 3rem;
     color: var(--text-color);
+    width: 100%;
   }
 `;
 
 export const NewNoteBox = styled.div`
-  width: 60%;
+  width: 100%;
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
 
   input,
   button {
     font-size: 1.2rem;
-    padding: 0.25rem 1rem;
     border: 0;
     height: 3rem;
+  }
+
+  button {
+    min-width: 3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   input {
     width: 30rem;
     border-radius: 0.25rem;
-    margin-right: 0.5rem;
+    padding: 0.25rem 1rem;
+    margin: 0 1rem;
   }
 
   button {
     border-radius: 5rem;
     background: var(--purple);
     color: #fff;
-
     transition: 0.2s;
+    margin-right: 1rem;
 
     &:hover {
       background: ${darken(0.1, "#5b5f97")};
--- src/components/NotesList/styles.ts
+++ src/components/NotesList/styles.ts
@@ -1,14 +1,14 @@
+import EdiText from "react-editext";
 import styled from "styled-components";
-import { darken } from "polished";
 
 export const Container = styled.section`
-  height: 500px;
+  min-height: 0px;
 
   margin: -5rem auto;
   padding: 3rem;
   width: 100%;
   background: var(--list-bg);
-  max-width: 1280px;
+  max-width: 800px;
   border-radius: 1rem;
 
   ul {
@@ -20,25 +20,34 @@ export const NoteItem = styled.li`
   background: #fff;
   border-radius: 0.25rem;
   min-height: 5rem;
-  display: flex;
-  align-items: center;
   padding: 1rem 2rem;
-  justify-content: space-between;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
 
   & + li {
     margin-top: 1rem;
   }
 
-  div {
-    display: flex;
-    flex-direction: row;
-    margin-left: 0.7rem;
+  p {
+    color: var(--date-color);
+    font-style: italic;
+    font-size: 0.7rem;
+    margin-top: 0.5rem;
+    align-self: flex-end;
   }
 
   button {
     border: 0;
     padding: 0.5rem;
     border-radius: 50%;
+    height: 2.5rem;
+    width: 2.5rem;
 
     transition: filter 0.2s;
   }
@@ -49,10 +58,38 @@ export const NoteItem = styled.li`
 
   button.delete {
     background: var(--red);
+    color: #fff;
+  }
+`;
+
+export const StyledEdiText = styled(EdiText)`
+  width: 100%;
+
+  button {
+    margin-right: 0.25rem;
   }
 
-  button.edit {
-    background: var(--light-purple);
-    margin-right: 0.5rem;
+  .edit {
+    color: var(--purple);
+    background-color: #fff;
+  }
+
+  .cancel {
+    color: var(--red);
+  }
+
+  .save {
+    color: var(--green);
+  }
+
+  .styles-module_Editext__cancel_button__26sqr::before {
+    content: "";
+    margin: 0;
+  }
+
+  .view-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
   }
 `;
